e51b632a3a1588ea8d1113aa0db9943d43e29c97
al <cangzan@gmail.com>
2021-12-30 11:56:40 +0800

    feat: save preferences.

--- champr/src/champr.rs
+++ champr/src/champr.rs
@@ -1,16 +1,22 @@
 use std::{borrow::Cow, cell::RefCell, collections::HashMap};
 
-use eframe::egui::{FontDefinitions, FontFamily, Ui, Checkbox, TextStyle};
+use eframe::egui::{Checkbox, CtxRef, FontDefinitions, FontFamily, TextStyle, TopBottomPanel, Ui};
 use serde::{Deserialize, Serialize};
+
+const CHAMPR_APP: &str = "champ-r";
 
 #[derive(Serialize, Deserialize)]
 pub struct Config {
     pub dark_mode: bool,
+    pub sources: Vec<String>,
 }
 
 impl Default for Config {
     fn default() -> Self {
-        Self { dark_mode: false }
+        Self {
+            dark_mode: false,
+            sources: vec![],
+        }
     }
 }
 
@@ -25,25 +31,29 @@ impl SourceItem {
     }
 }
 pub struct ChampR {
-    pub sources: RefCell<Vec<SourceItem>>,
+    pub source_list: RefCell<Vec<SourceItem>>,
     pub selected_sources: RefCell<HashMap<String, bool>>,
     pub config: Config,
 }
 
 impl ChampR {
     pub fn new() -> ChampR {
-        let sources: RefCell<Vec<SourceItem>> = RefCell::new(Vec::new());
+        let source_list: RefCell<Vec<SourceItem>> = RefCell::new(Vec::new());
         for i in 1..50 {
             let name = format!("source {}", i);
             let value = format!("source-{}", i);
             let item = SourceItem::new(name, value);
-            sources.borrow_mut().push(item);
+            source_list.borrow_mut().push(item);
         }
 
-        let config: Config = confy::load("champ-r").unwrap_or_default();
+        let config: Config = confy::load(CHAMPR_APP).unwrap_or_default();
+        let mut selected_sources = HashMap::new();
+        config.sources.iter().for_each(|k| {
+            selected_sources.insert(String::from(k), true);
+        });
         ChampR {
-            sources,
-            selected_sources: RefCell::new(HashMap::new()),
+            source_list,
+            selected_sources: RefCell::new(selected_sources),
             config,
         }
     }
@@ -71,11 +81,11 @@ impl ChampR {
     }
 
     pub fn render_list(&self, ui: &mut Ui) {
-        let sources = self.sources.borrow();
-        for s in sources.iter() {
+        let list = self.source_list.borrow();
+        let mut selected = self.selected_sources.borrow_mut();
+        for s in list.iter() {
             ui.group(|ui| {
-                let mut sources = self.selected_sources.borrow_mut();
-                let cur = sources.get(&s.value);
+                let cur = selected.get(&s.value);
                 let mut checked = match cur {
                     Some(true) => true,
                     _ => false,
@@ -84,42 +94,52 @@ impl ChampR {
                 let c = Checkbox::new(&mut checked, &s.name).text_style(TextStyle::Body);
                 let cb = ui.add(c);
                 if cb.clicked() {
-                    // `checked` is mutated after `click`
-                    sources.insert(String::from(&s.value), checked);
+                    selected.insert(s.value.to_string(), checked);
+                    save_config(&self.config, selected.iter());
                 }
             });
             ui.add_space(5.);
         }
     }
 
-    pub fn save_config(&self) {
-        let r = confy::store(
-            "champ-r",
-            Config {
-                dark_mode: self.config.dark_mode,
-            },
-        );
-        match r {
-            Ok(_) => println!("config saved successfully"),
-            Err(e) => println!("save config failed: {}", e),
-        }
-    }
+    pub fn render_top(&mut self, ctx: &CtxRef, _frame: &mut eframe::epi::Frame<'_>) {
+        TopBottomPanel::top("top_panel").show(ctx, |ui| {
+            let btn = ui.button(match self.config.dark_mode {
+                false => "🌌",
+                true => "💡",
+            });
 
-    pub fn render_top(&mut self, ui: &mut Ui) {
-        let btn = ui.button(if self.config.dark_mode {
-            "🌚"
-        } else {
-            "🌝"
+            if btn.clicked() {
+                self.config.dark_mode = !self.config.dark_mode;
+                save_config(&self.config, self.selected_sources.borrow().iter());
+            }
+
+            ui.label("The quick brown fox jumps over the lazy dog");
+            ui.label("敏捷的棕色狐狸跨过懒狗");
+            ui.separator();
+            ui.heading("This is heading #1");
+            ui.separator();
         });
-        if btn.clicked() {
-            self.config.dark_mode = !self.config.dark_mode;
-            self.save_config();
-        }
+    }
+}
 
-        ui.label("The quick brown fox jumps over the lazy dog");
-        ui.label("敏捷的棕色狐狸跨过懒狗");
-        ui.separator();
-        ui.heading("This is heading #1");
-        ui.separator();
+fn save_config(config: &Config, source_iter: std::collections::hash_map::Iter<String, bool>) {
+    let mut sources: Vec<String> = vec![];
+    source_iter
+        .filter(|&(_k, v)| *v == true)
+        .for_each(|(k, _v)| {
+            sources.push(String::from(k));
+        });
+
+    let r = confy::store(
+        CHAMPR_APP,
+        Config {
+            dark_mode: config.dark_mode,
+            sources,
+        },
+    );
+    match r {
+        Ok(_) => println!("config saved successfully"),
+        Err(e) => println!("save config failed: {}", e),
     }
 }
--- champr/src/main.rs
+++ champr/src/main.rs
@@ -2,7 +2,7 @@ pub mod champr;
 
 use champr::ChampR;
 use eframe::{
-    egui::{CentralPanel, ScrollArea, Vec2, Visuals},
+    egui::{CentralPanel, CtxRef, Hyperlink, ScrollArea, TextStyle, TopBottomPanel, Vec2, Visuals, Label},
     epi::App,
     run_native, NativeOptions,
 };
@@ -17,7 +17,7 @@ impl App for ChampR {
         self.configure_fonts(ctx);
     }
 
-    fn update(&mut self, ctx: &eframe::egui::CtxRef, _frame: &mut eframe::epi::Frame<'_>) {
+    fn update(&mut self, ctx: &eframe::egui::CtxRef, frame: &mut eframe::epi::Frame<'_>) {
         ctx.request_repaint();
 
         if self.config.dark_mode {
@@ -26,17 +26,33 @@ impl App for ChampR {
             ctx.set_visuals(Visuals::light());
         }
 
+        self.render_top(ctx, frame);
         CentralPanel::default().show(ctx, |ui| {
-            self.render_top(ui);
             ScrollArea::auto_sized().show(ui, |ui| {
                 self.render_list(ui);
             });
+            render_footer(ctx);
         });
     }
 
     fn name(&self) -> &str {
         "ChampR"
     }
+}
+
+fn render_footer(ctx: &CtxRef) {
+    TopBottomPanel::bottom("footer").show(ctx, |ui| {
+        ui.vertical_centered(|ui| {
+            ui.add_space(5.);
+            ui.add(Label::new("ChampR rust version"));
+            ui.add(
+                Hyperlink::new("https://github.com/cangzhang?from=rust_ver")
+                    .text("Made with 💛 by al")
+                    .text_style(TextStyle::Monospace),
+            );
+            ui.add_space(5.);
+        })
+    });
 }
 
 fn main() {
