c7b98525029584c6da9bd88b48f2a9b63d7afa3a
al <cangzan@gmail.com>
2022-02-16 01:06:35 +0800

    change(cmd): get output from pwsh.

--- cli/src/cmd.rs
+++ cli/src/cmd.rs
@@ -1,30 +1,29 @@
-use std::{process::Command, time::Duration};
+use std::{env, fs, process::Command, time::Duration};
 
 use anyhow::Result;
 use tokio::{task, time};
 
 pub async fn get_lcu_auth() -> Result<()> {
     let forever = task::spawn(async {
-        let mut interval = time::interval(Duration::from_secs(10));
+        let mut interval = time::interval(Duration::from_secs(5));
 
         loop {
             interval.tick().await;
 
-            let cmd_str = r#"
-            $tmp = new-item -path $env:temp -name "lcu_cmd_line.json" -force
-            Start-Process powershell -Verb runAs -ArgumentList "Get-CimInstance Win32_Process -Filter \""name = 'LeagueClientUx.exe'\"" | Select-Object CommandLine | ConvertTo-Json | out-file $tmp" -WindowStyle hidden
-            get-content -path $tmp
-            "#;
+            let tmp = env::temp_dir();
+            let output_file = tmp.join("lcu_cmd_line.json");
+            let cmd_str = format!(
+                r#"Start-Process powershell -Verb runAs -ArgumentList "Get-CimInstance Win32_Process -Filter \""name = 'LeagueClientUx.exe'\"" | Select-Object CommandLine | ConvertTo-Json | out-file -encoding utf8 -force {}" -WindowStyle hidden"#,
+                tmp.join("lcu_cmd_line.json").display()
+            );
 
-            println!("{}", cmd_str);
             let output = Command::new("powershell")
                 .args(["/C", &cmd_str])
                 .output()
                 .expect("failed to run powershell");
 
-            println!("status: {}", output.status);
-            println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
-            println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+            let file_content = fs::read_to_string(&output_file).unwrap();
+            println!("{}", file_content);
         }
     });
 
